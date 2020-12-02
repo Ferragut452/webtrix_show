a60f7ac8479efa859e41e98f6371c70eab8535d5
Oleg <oleg@wear-studio.com>
2020-12-02 13:10:26 +0200

    start

---
+++ js/about.1b560fd1.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"3f8f":function(c,a,n){"use strict";n.r(a);var t=n("7a23"),e=Object(t["y"])("data-v-0c6d789a");Object(t["r"])("data-v-0c6d789a");var r={class:"pricing"},d=Object(t["f"])("h2",null,"Pricing",-1);Object(t["p"])();var i=e((function(c,a,n,e,i,o){return Object(t["o"])(),Object(t["c"])("div",r,[d])})),o={name:"Pricing",components:{},data:function(){return{}}};o.render=i,o.__scopeId="data-v-0c6d789a";a["default"]=o}}]);
+//# sourceMappingURL=about.1b560fd1.js.map
